d89cdd793385d5a93f3b25dd00e5caa4a26e9d90
Ross Bulat <ross@jkrbinvestments.com>
2019-05-27 12:35:38 +0700

    tidy up

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -2,15 +2,10 @@ import React from "react";
 import styled, { ThemeProvider } from 'styled-components';
 import { backgroundColor, textColor } from './theme';
 
-// initiate theme context
-// default value only used when a component does not have a matching Provider above it in the tree
 const ThemeToggleContext = React.createContext();
 
-// exportable theme state
 export const useTheme = () => React.useContext(ThemeToggleContext);
 
-
-// exportable ThemeContext.Provider with current theming
 export const MyThemeProvider = ({ children }) => {
 
   const [themeState, setThemeState] = React.useState({
