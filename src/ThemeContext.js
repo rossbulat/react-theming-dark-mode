afe1e8465a1cb11795b2326de7e017f4661ffe24
siddhesh <iamsiddhesh22@gmail.com>
2021-05-08 15:24:51 +0530

    app mode stored in localstorage

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,16 +1,13 @@
-import React from "react";
-import styled, { ThemeProvider } from 'styled-components';
-import { backgroundColor, textColor } from './theme';
+import React, { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { backgroundColor, textColor } from "./theme";
 
 const ThemeToggleContext = React.createContext();
 
 export const useTheme = () => React.useContext(ThemeToggleContext);
 
 export const MyThemeProvider = ({ children }) => {
-
-  const [themeState, setThemeState] = React.useState({
-    mode: 'light'
-  });
+  const [themeState, setThemeState] = useState({ mode: "light" });
 
   const Wrapper = styled.div`
     background-color: ${backgroundColor};
@@ -18,20 +15,28 @@ export const MyThemeProvider = ({ children }) => {
   `;
 
   const toggle = () => {
-    const mode = (themeState.mode === 'light' ? `dark` : `light`);
+    const mode = themeState.mode === "light" ? `dark` : `light`;
     setThemeState({ mode: mode });
+    //this snippet saves saves themestate mode in localStorage
+    window.localStorage.setItem("mode", mode);
   };
+
+  //This Hook fetches the mode from localStorage and sets ThemeState
+  //if you comment this useEffect,the mode of this app will be light mode
+  //ie., if the page gets reload, this react app will be in light mode
+  useEffect(() => {
+    let localTheme = window.localStorage.getItem("mode");
+    localTheme && setThemeState({ mode: localTheme });
+  }, []);
 
   return (
     <ThemeToggleContext.Provider value={{ toggle: toggle }}>
       <ThemeProvider
         theme={{
-          mode: themeState.mode
+          mode: themeState.mode,
         }}
       >
-        <Wrapper>
-          {children}
-        </Wrapper>
+        <Wrapper>{children}</Wrapper>
       </ThemeProvider>
     </ThemeToggleContext.Provider>
   );
